539ad023ad1308b72eb0c3cf887fc4a353ce3763
akshaya <23951a04b0@iare.ac.in>
2025-09-06 11:32:44 +0530

    change in home page

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -53,7 +53,7 @@ const Home = () => {
               marginTop: "1rem",
             }}
           >
-            Your App Name
+            Akshaya url shortener
           </Title>
 
           <Text

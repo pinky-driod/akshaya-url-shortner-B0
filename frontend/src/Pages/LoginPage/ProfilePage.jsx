9ab8021b237a57a0dd4e12f075dd10a1be1d268d
akshaya <23951a04b0@iare.ac.in>
2025-09-07 16:21:49 +0530

    Backend successful

---
+++ frontend/src/Pages/LoginPage/ProfilePage.jsx
@@ -0,0 +1,40 @@
+import { Avatar } from '@mantine/core';
+import React, { useEffect, useState } from 'react'
+import { Stack} from '@mantine/core';
+import { Text } from '@mantine/core';
+import Service from '../utils/http';
+const service = new Service()
+
+
+export default function ProfilePage() {
+    const[user,setUser] = useState({});
+    async function getMyData() {
+        try{
+            let data = await service.get("user/me");
+            setUser(data);
+        }
+        catch(error){
+            console.log(error);
+        }
+    }
+  useEffect(()=>{
+      getMyData();
+  },[])
+  return ( 
+    <div>
+        <Stack
+      h={300}
+      bg="var(--mantine-color-body)"
+      align="center"
+      justify="center"
+      gap="md"
+    >
+        <Text size="lg">This is my profilePage</Text>
+        <Avatar src={user.avatar} alt = "it's me"/>
+        <Avatar color="green" radius={"xl"}></Avatar>
+        <Text>{user.name}</Text>
+        <Text>{user.email}</Text>
+        </Stack>
+    </div>
+  )
+}
